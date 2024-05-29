682078e514668cc26bd2f302849d08ae8acaeaa4
fekner <31614519+fekner@users.noreply.github.com>
2024-05-30 00:21:27 +0200

    Add files via upload

---
+++ Code/merging.sql
@@ -0,0 +1,42 @@
+
+SELECT
+  date_created,
+  contacts_n,
+  contacts_n_1,
+  contacts_n_2,
+  contacts_n_3,
+  contacts_n_4,
+  contacts_n_5,
+  contacts_n_6,
+  contacts_n_7,
+  new_type,
+  new_market
+FROM 'infra-affinity-408220.fiber.market1'
+UNION ALL
+SELECT
+  date_created,
+  contacts_n,
+  contacts_n_1,
+  contacts_n_2,
+  contacts_n_3,
+  contacts_n_4,
+  contacts_n_5,
+  contacts_n_6,
+  contacts_n_7,
+  new_type,
+  new_market
+FROM 'infra-affinity-408220.fiber.market2'
+UNION ALL
+SELECT
+  date_created,
+  contacts_n,
+  contacts_n_1,
+  contacts_n_2,
+  contacts_n_3,
+  contacts_n_4,
+  contacts_n_5,
+  contacts_n_6,
+  contacts_n_7,
+  new_type,
+  new_market
+FROM 'infra-affinity-408220.fiber.market3'
